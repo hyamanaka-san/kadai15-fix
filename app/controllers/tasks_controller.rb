c1bc32e7af6f770723efadc790a81117fe4bccfe
hyamanaka-san <hdtymnk@gmail.com>
2019-08-06 07:40:39 +0000

    task_kadai

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,13 @@
 class TasksController < ApplicationController
+    before_action :set_task, only: [:show, :edit, :update, :destroy]
+    
+    
     def index
         @tasks = Task.all
     end
     
     def show
-        @task = Task.find(params[:id])
+        
     end
 
     def new
@@ -27,12 +30,12 @@ class TasksController < ApplicationController
     end
     
     def edit 
-        @task = Task.find(params[:id])
+       
     end    
     
     
     def update
-        @task = Task.find(params[:id])
+        
         
         if @task.update(task_params)
            flash[:success] = 'task が正常に更新されました'
@@ -45,7 +48,7 @@ class TasksController < ApplicationController
     end
     
     def destroy
-        @task = Task.find(params[:id])
+        
         @task.destroy
         #Task.find(params[:id]).destroy
         
@@ -54,9 +57,13 @@ class TasksController < ApplicationController
     end
 
     private
+    
+    def set_task
+        @task = Task.find(params[:id])
+    end
 
     def task_params
-        params.require(:task).permit(:content)
+        params.require(:task).permit(:content, :status)
     end
 
     
